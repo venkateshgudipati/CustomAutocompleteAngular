9d8628dc1d45821d7748aff8624ada4777939efe
Venkatesh Gudapati <venkatesh.gudapati@trigyn.com>
2019-08-13 12:32:58 +0530

    integrated autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { debounceTime, distinctUntilChanged, switchMap,catchError, map } from 'rxjs/operators';
+import { Observable, Subject, of } from 'rxjs';
+import { AutocompleteService } from './autocomplete.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public clients: Observable<any[]>;
+  private searchTerms = new Subject<string>();
+  public ClientName = '';
+  public flag: boolean = true;
   title = 'autocomplete';
-}
+  constructor(
+    private clientSearchService: AutocompleteService,
+  ) {}
+
+
+    ngOnInit(): void {
+      this.clients = this.searchTerms.pipe(
+        debounceTime(300),       // wait for 300ms pause in events  
+        distinctUntilChanged(),   // ignore if next search term is same as previous  
+        switchMap(term =>  term  // switch to new observable each time  
+          // return the http search observable  
+          ? this.clientSearchService.searchData(term)
+          : []
+        ),
+          // or the observable of empty heroes if no search term  
+          catchError(error => {
+          // TODO: real error handling  
+          console.log(error);
+          return [];
+        }));
+    }
+    // Push a search term into the observable stream.  
+    searchClient(term: string): void {
+      this.flag = true;
+      this.searchTerms.next(term);
+    }
+    onselectClient(ClientObj) {
+      if (ClientObj.ClientId != 0) {
+        this.ClientName = ClientObj.text;
+        this.flag = false;
+      }
+      else {
+        return false;
+      }
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http'
+import { ReactiveFormsModule,FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwtInterceptor } from './Jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
